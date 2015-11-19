d5898dda99780e6bd351139e8ccb228077bfe3c9
amgadali <amgad_gis@yahoo.com>
2015-11-19 21:14:52 +0300

    Initial

---
+++ ANSI2UNICODE/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace ANSI2UNICODE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtOriginal = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtConverted = new System.Windows.Forms.TextBox();
+            this.cboEncoding = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(404, 24);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(78, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Path:";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(342, 56);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(140, 23);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Save Encoded File";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(97, 24);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(301, 22);
+            this.txtFilePath.TabIndex = 5;
+            // 
+            // txtOriginal
+            // 
+            this.txtOriginal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOriginal.Location = new System.Drawing.Point(3, 18);
+            this.txtOriginal.Multiline = true;
+            this.txtOriginal.Name = "txtOriginal";
+            this.txtOriginal.Size = new System.Drawing.Size(438, 406);
+            this.txtOriginal.TabIndex = 6;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 96);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(907, 427);
+            this.splitContainer1.SplitterDistance = 444;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtOriginal);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(444, 427);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Original Text";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtConverted);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(459, 427);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Converted Text";
+            // 
+            // txtConverted
+            // 
+            this.txtConverted.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConverted.Location = new System.Drawing.Point(3, 18);
+            this.txtConverted.Multiline = true;
+            this.txtConverted.Name = "txtConverted";
+            this.txtConverted.Size = new System.Drawing.Size(453, 406);
+            this.txtConverted.TabIndex = 7;
+            // 
+            // cboEncoding
+            // 
+            this.cboEncoding.FormattingEnabled = true;
+            this.cboEncoding.Location = new System.Drawing.Point(97, 56);
+            this.cboEncoding.Name = "cboEncoding";
+            this.cboEncoding.Size = new System.Drawing.Size(222, 24);
+            this.cboEncoding.TabIndex = 8;
+            this.cboEncoding.SelectedIndexChanged += new System.EventHandler(this.cboEncoding_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Encooding:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 538);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboEncoding);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TextBox txtOriginal;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtConverted;
+        private System.Windows.Forms.ComboBox cboEncoding;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ANSI2UNICODE/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ANSI2UNICODE
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EncodingInfo[] encoodingList = Encoding.GetEncodings();
+            foreach (EncodingInfo encoding in encoodingList)
+            {
+                ComboboxItem item = new ComboboxItem();
+                item.Text = encoding.Name +" - "+ encoding.DisplayName ;
+                item.Value = encoding.CodePage;
+                cboEncoding.Items.Add(item);
+            }
+
+        }
+        byte[] temp = null;
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtFilePath.Text = ofd.FileName;
+                if (File.Exists(ofd.FileName))
+                {
+                    temp = File.ReadAllBytes(ofd.FileName);
+                    txtOriginal.Text = File.ReadAllText(ofd.FileName);
+                }
+            }
+        }
+
+        private void cboEncoding_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboboxItem item = cboEncoding.SelectedItem as ComboboxItem;
+            if (item != null)
+            {
+                // temp  = Encoding.ASCII.GetBytes(txtOriginal.Text);
+                Encoding c1256 = System.Text.Encoding.GetEncoding(int.Parse( item.Value.ToString()));
+                char[] chars = c1256.GetChars(temp);
+                StringBuilder sb = new StringBuilder();
+                sb.Append(chars);
+                txtConverted.Text = sb.ToString();
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svd = new SaveFileDialog();
+            if (svd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamWriter sr = File.CreateText(svd.FileName);
+                sr.Write(txtConverted.Text.ToCharArray());
+                sr.Close();
+            }
+        }
+
+
+    }
+}
